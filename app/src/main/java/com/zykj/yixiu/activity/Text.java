d87dce85a34bbd3aa951e1080cc4ed3936bbca65
zykj <204628104@qq.com>
2017-04-11 12:59:42 +0800

    Initial commit

---
+++ app/src/main/java/com/zykj/yixiu/activity/Text.java
@@ -0,0 +1,18 @@
+package com.zykj.yixiu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zykj.yixiu.R;
+
+/**
+ * Created by zykj on 2017/4/11.
+ */
+
+public class Text extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
---
+++ app/src/main/java/com/zykj/yixiu/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.zykj.yixiu.app;
+
+import android.app.Application;
+
+/**
+ * Created by zykj on 2017/4/11.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
